61a18e7f22cd714a18df996e9591df32f491ae00
joao147 <50873876+joao147@users.noreply.github.com>
2020-06-15 12:57:29 -0400

    Adição de comentários
    Comentários para melhor entendimento do código

--- IA/src/Tabuleiro.js
+++ IA/src/Tabuleiro.js
@@ -18,36 +18,45 @@ function shuffle(array) {
 }
 
 class Tabuleiro{
-    tabuleiro= new Array();
+    tabuleiro= new Array();//Tabuleiro
     posicaoOuro;
-    
+    //CONSTANTES
+    NADA = 0;
+    POCO = 1;
+    WUMPUS = 2;
+    OURO = 3;
+    AGENTE = 4;
+
     constructor(){}
 
-    gerarTabuleiro(qtd) {
-        this.tabuleiro.push(4);// 4 representa o agente
-        this.tabuleiro.push(3);// 3 representa o ouro
-        this.tabuleiro.push(2);// 2 representa o wumpus
+    gerarTabuleiro(qtd){
+        this.tabuleiro.push(AGENTE);// 4 representa o agente
+        this.tabuleiro.push(OURO);// 3 representa o ouro
+        this.tabuleiro.push(WUMPUS);// 2 representa o wumpus
         
-        let i = 2;
-        for(let j=0; j<qtd; j++){
-            this.tabuleiro.push(1);
-            i++;
-        }
-        while(i<64){
-            this.tabuleiro.push(0);
+        let i = 2;//Inicia em 2 pois já há 3 itens no tabuleiro(agente, ouro e wumpus)
+
+        for(let j = 0; j < qtd; j++){//adiciona o numero de poços que o usuário inserir
+            this.tabuleiro.push(POCO);// 1 representa os poços
             i++;
         }
 
-        shuffle(this.tabuleiro);
+        while(i<64){//Inseri NADA no resto do tabuleiro
+            this.tabuleiro.push(NADA);// 0 representa que não a nada que impeça o agente
+            i++;
+        }
 
-        const lugar = this.tabuleiro.indexOf(4);
-        this.tabuleiro[lugar] = this.tabuleiro[0];
-        this.tabuleiro[0] = 4;
-        this.posicaoOuro = this.tabuleiro.indexOf(3);
-        
+        shuffle(this.tabuleiro);//Aplica o shuffle no array
+
+        //O agente sempre inicia no [0,0]
+        const lugar = this.tabuleiro.indexOf(4);//recebe posição do agente após shuffle
+        this.tabuleiro[lugar] = this.tabuleiro[0];//adiciona o que estava na posição [0,0] a posição onde o agente estava
+        this.tabuleiro[0] = 4;//Coloca o agente na posição[0,0]
+
+        this.posicaoOuro = this.tabuleiro.indexOf(3);//resgata a posição do ouro no array
     }
 
-    printTabuleiro(){
+    printTabuleiro(){//mostra o tabuleiro
         let i=0;
         while(i<8){
             console.log(`${this.tabuleiro[(i*8)+0]} ${this.tabuleiro[(i*8)+1]} ${this.tabuleiro[(i*8)+2]} ${this.tabuleiro[(i*8)+3]} ${this.tabuleiro[(i*8)+4]} ${this.tabuleiro[(i*8)+5]} ${this.tabuleiro[(i*8)+6]} ${this.tabuleiro[(i*8)+7]}`)
@@ -56,6 +65,6 @@ class Tabuleiro{
     }
 }
 
-const tab = new Tabuleiro();
+const tab = new Tabuleiro();//Cria um objeto de Tabuleiro
 
-module.exports.tab = tab;
+module.exports.tab = tab;//exporta o objeto cria 'tab'
